c124baca6a28e4fe3906b44d3adc513bfa2e22f9
Berujka <beraayse@gmail.com>
2020-05-24 17:34:16 +0300

    activity_main2.xml completed

---
+++ app/src/main/java/com/beraa/myfavoritebooks/Main2Activity.java
@@ -0,0 +1,26 @@
+package com.beraa.myfavoritebooks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        
+    }
+
+    public void selectImage(View view){
+
+    }
+
+    public void save(View view){
+
+    }
+
+}
